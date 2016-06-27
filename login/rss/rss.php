8c9d76c3695730d6bbad754cf0381171c61db48b
maoda <maoda@yahoo-corp.jp>
2016-06-27 14:00:59 +0900

    first commit

---
+++ login/rss/rss.php
@@ -0,0 +1,88 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+echo '
+{
+  "feedUrl":"http://design01.login.ssk.ynwm.yahoo.co.jp/users/maoda/study/xml/rss.xml",
+  "title":"Yahoo!ニュース・トピックス - トップ",
+  "link":"http://news.yahoo.co.jp/",
+  "author":"",
+  "description":"Yahoo! JAPANのニュース・トピックスで取り上げている最新の見出しを提供しています。",
+  "type":"rss20",
+  "entries":[
+    {
+      "title":"政権正念場 ジョーカー切るか",
+      "link":"http://news.yahoo.co.jp/pickup/6193720",
+      "author":"",
+      "publishedDate":"Mon, 07 Mar 2016 13:22:41 +0900",
+      "contentSnippet":"",
+      "content":"",
+      "categories":""
+    },
+    {
+      "title":"田母神氏の関連事務所を捜索",
+      "link":"http://news.yahoo.co.jp/pickup/6193730",
+      "author":"",
+      "publishedDate":"Mon, 07 Mar 2016 14:35:33 +0900",
+      "contentSnippet":"",
+      "content":"",
+      "categories":""
+    },
+    {
+      "title":"経済苦で高校中退 5000人超",
+      "link":"http://news.yahoo.co.jp/pickup/6193735",
+      "author":"",
+      "publishedDate":"Mon, 07 Mar 2016 15:32:10 +0900",
+      "contentSnippet":"",
+      "content":"",
+      "categories":""
+    },
+    {
+      "title":"保育園落ちた 政争ではない?",
+      "link":"http://news.yahoo.co.jp/pickup/6193742",
+      "author":"",
+      "publishedDate":"Mon, 07 Mar 2016 16:21:08 +0900",
+      "contentSnippet":"",
+      "content":"",
+      "categories":""
+    },
+    {
+      "title":"トランプ氏 改めて拷問を支持",
+      "link":"http://news.yahoo.co.jp/pickup/6193721",
+      "author":"",
+      "publishedDate":"Mon, 07 Mar 2016 15:12:22 +0900",
+      "contentSnippet":"",
+      "content":"",
+      "categories":""
+    },
+    {
+      "title":"日本人2選手 MLBで輝く40代",
+      "link":"http://news.yahoo.co.jp/pickup/6193743",
+      "author":"",
+      "publishedDate":"Mon, 07 Mar 2016 16:17:11 +0900",
+      "contentSnippet":"",
+      "content":"",
+      "categories":""
+    },
+    {
+      "title":"シャラポワ引退か 重大発表へ",
+      "link":"http://news.yahoo.co.jp/pickup/6193722",
+      "author":"",
+      "publishedDate":"Mon, 07 Mar 2016 13:37:35 +0900",
+      "contentSnippet":"",
+      "content":"",
+      "categories":""
+    },
+    {
+      "title":"めちゃイケ継続 視聴率に注文",
+      "link":"http://news.yahoo.co.jp/pickup/6193745",
+      "author":"",
+      "publishedDate":"Mon, 07 Mar 2016 16:43:51 +0900",
+      "contentSnippet":"",
+      "content":"",
+      "categories":""
+    }
+  ]
+}
+';
+?>
---
+++ login/zaseki/zaseki.php
@@ -0,0 +1,18 @@
+<?php 
+header("Content-Type: application/json; charset=UTF-8");
+
+echo '
+[
+  {"name":"yamada", "seat":"36F",  "id":1111},
+  {"name":"tanaka", "seat":"N-01", "id":2222},
+  {"name":"satou",  "seat":"S-07", "id":3333},
+  {"name":"itirou", "seat":"AAA",  "id":4444},
+  {"name":"zijou",  "seat":"BBB",  "id":5555},
+  {"name":"saburou",  "seat":"CCC",  "id":6666},
+  {"name":"shirou",  "seat":"DDD",  "id":7777},
+  {"name":"gorou",  "seat":"EEE",  "id":8888},
+  {"name":"mutugorou",  "seat":"FFF",  "id":9999}
+]
+';
+
+?>
